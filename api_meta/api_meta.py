cf434f033441c88aa54be762e0aa6dea72fd3a70
Daniel Lourenço <119344845+flourenco-daniel@users.noreply.github.com>
2024-06-11 17:27:31 -0300

    Update api_meta.py
    deleting token and account id

--- api_meta/api_meta.py
+++ api_meta/api_meta.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-access_token = 'EAAUeZC6mFhnYBOysyZALP8ZCKWjfzrvVnnU8ET6xcQaZBllHIkdvVjlXL1arNWYdDT8qaEm9x1WWX48auHuGow53cfSBZCBsgZCaFcAw6F7UM8gS5wFFMeqoBmTQ7Tn5I1REICPFLIG30KoWCqMDrllfEAOGCgHVDnrsT7IlhVmqXtJIDVp392EyCyqsinhJBEc7soJ5xuiycVIliB7Sa6yKj7ggZAZCHSF6MjYZD'
+access_token = ''
 
-account_id = '170578383605870'
+account_id = ''
 
 url = f'https://graph.facebook.com/v20.0/act_{account_id}/ads'
 
